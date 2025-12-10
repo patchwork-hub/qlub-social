409cae131f22994b0e7d38bb1448ac874dd48df4
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-12-10 22:27:39 +0630

    Rollback code into mastodon native

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -52,8 +52,7 @@ class StatusesIndex < Chewy::Index
     },
   }
 
-  # CUSTOMIZED CODE < Adding index_scope to without_banned from content_filters gem >
-  index_scope ::Status.unscoped.kept.without_reblogs.without_banned.includes(:media_attachments, :local_mentioned, :local_favorited, :local_reblogged, :local_bookmarked, :tags, preview_cards_status: :preview_card, preloadable_poll: :local_voters), delete_if: ->(status) { status.searchable_by.empty? }
+  index_scope ::Status.unscoped.kept.without_reblogs.includes(:media_attachments, :local_mentioned, :local_favorited, :local_reblogged, :local_bookmarked, :tags, preview_cards_status: :preview_card, preloadable_poll: :local_voters), delete_if: ->(status) { status.searchable_by.empty? }
 
   root date_detection: false do
     field(:id, type: 'long')
